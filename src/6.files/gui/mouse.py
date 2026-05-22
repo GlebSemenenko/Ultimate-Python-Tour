fb2c67e79ade9ca3e2ce9afa1abeaf58526d4980
Gleb <semenenkogleb195@gmail.com>
2026-05-22 10:21:51 +0300

    22-05-2026

--- src/6.files/gui/mouse.py
+++ src/6.files/gui/mouse.py
@@ -1,13 +1,39 @@
+import random
+import time
 import pyautogui
 
-
-x, y = pyautogui.position()
-print(f"Текущие координаты курсора: X={x}, Y={y}")
-
-
-coorddinates = {
-    1,2
-    1,3
+# Словарь с координатами (обратите внимание: нет ключа 4)
+coordinates = {
+    1: [237, 194],
+    2: [478, 197],
+    3: [614, 201],
+    4: [615, 204],
+    5: [798, 202],
+    6: [881, 191],
+    7: [951, 202],
+    8: [1211, 200],
+    9: [1407, 194],
 }
 
-#def auto()
+# Создаём список существующих ключей, чтобы не выпадала ошибка при random
+valid_keys = list(coordinates.keys())
+
+print("Запуск автоматического кликера. Для остановки нажмите Ctrl+C в терминале.\n")
+
+try:
+    while True:
+        # Выбираем случайный ключ из существующих (1,2,3,5,6,7,8,9)
+        cor = random.choice(valid_keys)
+        # Получаем координаты
+        x, y = coordinates[cor]  # x = coordinates[cor][0], y = coordinates[cor][1]
+
+        # Печатаем, куда сейчас будем кликать
+        print(f"Клик по точке {cor}: X={x}, Y={y}")
+        # Выполняем клик (не присваиваем результат, т.к. click() возвращает None)
+        pyautogui.click(x, y)
+
+        randomtime = random.randint(4, 58)
+        time.sleep(randomtime)
+
+except KeyboardInterrupt:
+    print("\nПрограмма остановлена пользователем.")
